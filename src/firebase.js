95ff9ab66b147ea13cfefa2a9f4fcd0f51359b97
ashirbad behera <ashirbadbehera29@gmail.com>
2020-09-11 10:26:00 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyAKbAoYiAeDj7XAQkdgNhRQOA9b39bYYno',
+	authDomain: 'whatsapp-clone-11.firebaseapp.com',
+	databaseURL: 'https://whatsapp-clone-11.firebaseio.com',
+	projectId: 'whatsapp-clone-11',
+	storageBucket: 'whatsapp-clone-11.appspot.com',
+	messagingSenderId: '821031505611',
+	appId: '1:821031505611:web:d6fdd2a9a799d4b59ad54e',
+	measurementId: 'G-002HCHPEKR',
+};
