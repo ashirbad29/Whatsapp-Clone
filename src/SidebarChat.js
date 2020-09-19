8fa75d800c3eea81d8bda9622d872e6fcfa1f35a
ashirbad behera <ashirbadbehera29@gmail.com>
2020-09-19 16:30:27 +0530

    fiexed a minor bug & styling of the links

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -18,7 +18,7 @@ function SidebarChat({ id, name, addNewChat }) {
 					setMessages(snapshot.docs.map(doc => doc.data()))
 				);
 		}
-	});
+	}, [id]);
 
 	// to generate a random avatar
 	useEffect(() => {
